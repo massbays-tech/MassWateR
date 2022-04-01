4220d6a6ae0639849e61254ee6cb52f8accb31bf
fawda123 <mbafs2012@gmail.com>
2022-04-01 11:28:57 -0400

    adding example datasets and initial doc

---
+++ R/exresults.R
@@ -0,0 +1,17 @@
+#' Example file of water quality results
+#' 
+#' Example file of water quality results 
+#' 
+#' @format A \code{data.frame} of 2584 rows and 14 columns
+#' 
+#' @details An example of water quality data formatted for input in the QAQC or exploratory analysis workflow
+#' 
+#' @examples 
+#' \dontrun{
+#' library(readxl)
+#' 
+#' exresults <- read_excel('inst/extdata/ExampleResults_final.xlsx')
+#' 
+#' save(exresults, file = 'data/exresults.RData', compress = 'xz')
+#' }
+"exresults"
