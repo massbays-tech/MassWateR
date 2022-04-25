bb49ea34cdbc0e61ac928426518722deb88ddfab
fawda123 <mbafs2012@gmail.com>
2022-04-24 21:21:38 -0400

    fixing checks and bugs

--- R/globalVariables.R
+++ R/globalVariables.R
@@ -1,4 +1,4 @@
 globalVariables(c("Activity Start Date", "Activity Start Time", "QC Reference Value", "Result Unit",
                   "Parameter", "...7", "MDL", "UQL", "uom", "Monitoring Location Latitude", 
                   "Monitoring Location Longitude", "Characteristic Name", "obs", "percent", 
-                  "standard"))
+                  "standard", "check", "met", "name", "value"))
