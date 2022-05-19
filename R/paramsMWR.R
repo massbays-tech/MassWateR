49192504f22071eb782800550957418f791fbe34
fawda123 <mbafs2012@gmail.com>
2022-05-19 16:21:14 -0400

    updating parameter mapping source and package file with new method column

--- R/paramsMWR.R
+++ R/paramsMWR.R
@@ -2,9 +2,9 @@
 #' 
 #' Master parameter list and units for Characteristic Name column in results data
 #' 
-#' @format A \code{data.frame} of 39 rows and 5 columns
+#' @format A \code{data.frame} of 39 rows and 6 columns
 #' 
-#' @details This information is used to verify the correct format of input results data in \code{\link{formMWRresults}}.  A column showing the corresponding WQX names is also included.
+#' @details This information is used to verify the correct format of input data and for minor formatting of output data for upload to WQX.  A column showing the corresponding WQX names is also included.
 #' 
 #' @examples 
 #' \dontrun{
@@ -12,7 +12,7 @@
 #' library(dplyr)
 #' 
 #' paramsMWR <- read_excel('inst/extdata/ParameterMapping_final.xlsx') %>% 
-#'   rename(Note = `...5`)
+#'   rename(Note = `...6`)
 #' 
 #' save(paramsMWR, file = 'data/paramsMWR.RData', compress = 'xz')
 #' }
