e627d37bae06ffcc52cc5fe6621fd89874d97155
fawda123 <mbafs2012@gmail.com>
2022-06-14 09:12:53 -0400

    call read_excel explicitly for tests to avoid load error

--- tests/testthat/helper-data.R
+++ tests/testthat/helper-data.R
@@ -1,20 +1,19 @@
 library(lubridate)
-library(readxl)
 library(dplyr)
 
 # results data
 respth <- system.file('extdata/ExampleResults.xlsx', package = 'MassWateR')
-resdat <- read_excel(respth, na = c('NA', 'na', ''), 
+resdat <- readxl::read_excel(respth, na = c('NA', 'na', ''), 
                              col_types = c('text', 'text', 'date', 'date', 'text', 'text', 'text', 'text', 'text', 'text',
                                            'text', 'text', 'text', 'text'))
 
 # dqo accuracy data
 accpth <- system.file('extdata/ExampleDQOAccuracy.xlsx', package = 'MassWateR')
-accdat <- read_excel(accpth, na = c('NA', 'na', ''))
+accdat <- readxl::read_excel(accpth, na = c('NA', 'na', ''))
 
 # dqo completeness data
 frecompth <- system.file('extdata/ExampleDQOFrequencyCompleteness.xlsx', package = 'MassWateR')
-frecomdat <- suppressMessages(read_excel(frecompth, 
+frecomdat <- suppressMessages(readxl::read_excel(frecompth, 
                           skip = 1, na = c('NA', 'na', ''), 
                           col_types = c('text', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric')
 )) %>% 
@@ -22,4 +21,4 @@ frecomdat <- suppressMessages(read_excel(frecompth,
 
 # site metadata
 sitpth <- system.file('extdata/ExampleSites.xlsx', package = 'MassWateR')
-sitdat <- read_excel(sitpth, na = c('NA', 'na', ''))
+sitdat <- readxl::read_excel(sitpth, na = c('NA', 'na', ''))
