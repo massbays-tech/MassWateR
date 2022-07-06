b413dd13ae92ff9877952dec060244b6c5d92a28
fawda123 <mbafs2012@gmail.com>
2022-07-06 10:40:16 -0400

    fixing builds for tests

--- tests/testthat/test-anlzMWRoutlier.R
+++ tests/testthat/test-anlzMWRoutlier.R
@@ -1,23 +1,28 @@
 test_that("Checking ouput format by month", {
-  result <- anlzMWRoutlier(res = resdat, param = 'DO', type = 'month')
+  result <- anlzMWRoutlier(res = resdat, param = 'DO', acc = accdat, type = 'month')
   expect_s3_class(result, 'ggplot')
 })
 
 test_that("Checking ouput format by site", {
-  result <- anlzMWRoutlier(res = resdat, param = 'DO', type = 'site')
+  result <- anlzMWRoutlier(res = resdat, param = 'DO', acc = accdat, type = 'site')
+  expect_s3_class(result, 'ggplot')
+})
+
+test_that("Checking ouput format log-scale", {
+  result <- anlzMWRoutlier(res = resdat, param = 'E.coli', acc = accdat, type = 'site')
   expect_s3_class(result, 'ggplot')
 })
 
 test_that("Error if parameter not found", {
-  expect_error(anlzMWRoutlier(res = resdat, param = 'notfound', type = 'month'))
+  expect_error(anlzMWRoutlier(res = resdat, param = 'notfound', acc = accdat, type = 'month'))
 })
 
 test_that("Return outliers only", {
-  result <- anlzMWRoutlier(res = resdat, param = 'DO', type = 'month', outliers = TRUE)
+  result <- anlzMWRoutlier(res = resdat, param = 'DO', acc = accdat, type = 'month', outliers = TRUE)
   expect_s3_class(result, 'data.frame')
 })
 
 test_that("Testing no repel for outliers", {
-  result <- anlzMWRoutlier(res = resdat, param = 'DO', type = 'month', repel = F)
+  result <- anlzMWRoutlier(res = resdat, param = 'DO', acc = accdat, type = 'month', repel = F)
   expect_s3_class(result, 'ggplot')
 })
