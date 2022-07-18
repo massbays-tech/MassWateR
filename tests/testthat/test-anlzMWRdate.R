b9f423d3ce57fbdb68e343ce4610bd4c40dcfd38
fawda123 <mbafs2012@gmail.com>
2022-07-18 08:37:31 -0400

    full test coverage on anlzMWRdate

--- tests/testthat/test-anlzMWRdate.R
+++ tests/testthat/test-anlzMWRdate.R
@@ -3,6 +3,11 @@ test_that("Checking output format group as site", {
   expect_s3_class(result, 'ggplot')
 })
 
+test_that("Checking output format group as site, repel = FALSE", {
+  result <- anlzMWRdate(res = resdat, param = 'DO', acc = accdat, group = 'site', repel = FALSE)
+  expect_s3_class(result, 'ggplot')
+})
+
 test_that("Checking output format group as all", {
   result <- anlzMWRdate(res = resdat, param = 'DO', acc = accdat, group = 'all')
   expect_s3_class(result, 'ggplot')
