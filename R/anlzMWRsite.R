b1ac58d24b2c8112611aec5919940307735e17b8
fawda123 <mbafs2012@gmail.com>
2022-07-25 09:32:06 -0400

    jitter types for anlzMWRsite #25

--- R/anlzMWRsite.R
+++ R/anlzMWRsite.R
@@ -7,13 +7,12 @@
 #' @param acc character string of path to the data quality objectives file for accuracy or \code{data.frame} returned by \code{\link{readMWRacc}}
 #' @param sit optional character string of path to the site metadata file or \code{data.frame} of site metadata returned by \code{\link{readMWRsites}}, required if \code{locgroup} is not \code{NULL} 
 #' @param thresh character indicating if relevant freshwater or marine threshold lines are included, one of \code{"fresh"}, \code{"marine"}, or \code{"none"}
-#' @param type character indicating \code{"box"} for boxplots or \code{"bar"} for barplots, see details
+#' @param type character indicating \code{"box"}, \code{"jitterbox"}, \code{"bar"}, \code{"jitterbar"} or \code{"jitter"}, see details
 #' @param threshcol character indicating color of threshold lines if available
 #' @param site character string of sites to include, default all
 #' @param resultatt character string of result attributes to plot, default all
 #' @param locgroup character string of location groups to plot from the \code{"Location Group"} column in the site metadata file, optional and only if \code{sit} is not \code{NULL}
 #' @param dtrng character string of length two for the date ranges as YYYY-MM-DD, default all
-#' @param jitter logical indicating if points are jittered over the boxplots, only applies if \code{type = "box"}
 #' @param confint logical indicating if confidence intervals are shown, only applies if \code{type = "bar"}
 #' @param fill numeric indicating fill color for boxplots or barplots
 #' @param alpha numeric from 0 to 1 indicating transparency of fill color
@@ -25,7 +24,7 @@
 #'
 #' @return A \code{\link[ggplot2]{ggplot}} object that can be further modified.
 #' 
-#' @details Summaries of a parameter for each site are shown as boxplots if \code{type = "box"} or as barplots if \code{type = "bar"}.  For the latter, points can be jittered over the boxplots by setting \code{jitter = TRUE}.  For the former, 95% confidence intervals are also shown if they can be estimated (i.e., more than one result value per bar). 
+#' @details Summaries of a parameter for each site are shown as boxplots if \code{type = "box"} or as barplots if \code{type = "bar"}.  Points can be jittered over the boxplots by setting \code{type = "jitterbox"} or jittered over the barplots by setting \code{type = "jitterbar"}.  Setting \code{type = "jitter"} will show only only the jittered points.  For \code{type = "bar"} or \code{type = "jitterbar"}, 95% confidence intervals can also be shown if \code{confint = TRUE} and they can be estimated (i.e., more than one result value per bar). 
 #'
 #' Threshold lines applicable to marine or freshwater environments can be included in the plot by using the \code{thresh} argument.  These thresholds are specific to each parameter and can be found in the \code{\link{thresholdMWR}} file.  Threshold lines are plotted only for those parameters with entries in \code{\link{thresholdMWR}} and only if the value in \code{`Result Unit`} matches those in \code{\link{thresholdMWR}}. The threshold lines can be suppressed by setting \code{thresh = 'none'}. 
 #'  
@@ -76,7 +75,7 @@
 #' anlzMWRsite(res = resdat, param = 'DO', acc = accdat, sit = sitdat, type = 'box', 
 #'      thresh = 'fresh', locgroup = 'Concord')
 #'      
-anlzMWRsite <- function(res, param, acc, sit = NULL, type = c('box', 'bar'), thresh, threshcol = 'tan', site = NULL, resultatt = NULL, locgroup = NULL, dtrng = NULL, jitter = FALSE, confint = FALSE, fill = 'lightgreen', alpha = 0.8, width = 0.8, yscl = c('auto', 'log', 'linear'), fecalgrp = FALSE, runchk = TRUE, warn = TRUE){
+anlzMWRsite <- function(res, param, acc, sit = NULL, type = c('box', 'jitterbox', 'bar', 'jitterbar', 'jitter'), thresh, threshcol = 'tan', site = NULL, resultatt = NULL, locgroup = NULL, dtrng = NULL, confint = FALSE, fill = 'lightgreen', alpha = 0.8, width = 0.8, yscl = c('auto', 'log', 'linear'), fecalgrp = FALSE, runchk = TRUE, warn = TRUE){
   
   fec <- c('E.coli', 'Enterococcus', 'Fecal Coliform')
   type <- match.arg(type)
@@ -137,9 +136,8 @@ anlzMWRsite <- function(res, param, acc, sit = NULL, type = c('box', 'bar'), thr
     
   }
   
-  
   # boxplot, no fecal group
-  if(type == 'box' & !fecalgrp){
+  if((type == 'box' | type == 'jitterbox') & !fecalgrp){
     
     toplo <- toplo %>% 
       dplyr::group_by(`Monitoring Location ID`) %>% 
@@ -155,7 +153,7 @@ anlzMWRsite <- function(res, param, acc, sit = NULL, type = c('box', 'bar'), thr
   }
   
   # boxplot, fecal group
-  if(type == 'box' & fecalgrp){
+  if((type == 'box' | type == 'jitterbox') & fecalgrp){
     
     toplo <- toplo %>% 
       dplyr::group_by(`Monitoring Location ID`, `Result Attribute`) %>% 
@@ -171,53 +169,79 @@ anlzMWRsite <- function(res, param, acc, sit = NULL, type = c('box', 'bar'), thr
     
   }
   
-  # barplot
-  if(type == 'bar' & !fecalgrp){
+  # jitter if box, not fecal group
+  if(type == 'jitterbox' & !fecalgrp){
     
-    toplo <- toplo %>% 
-      dplyr::group_by(`Monitoring Location ID`)
+    jitplo <- toplo %>% 
+      dplyr::filter(!outlier)
     
-    # get mean and CI summary
-    toplo <- utilMWRconfint(toplo, logscl = logscl)
-    
-    p <- p +
-      ggplot2::geom_bar(data = toplo, ggplot2::aes(x = `Monitoring Location ID`, y = `Result Value`),
-                        fill = fill, stat = 'identity', alpha = alpha, width = width)
-    
-    if(confint)
-      p <- p + 
-        ggplot2::geom_errorbar(data = toplo, ggplot2::aes(x = `Monitoring Location ID`, ymin = lov, ymax = hiv), width = 0.2 * width)
+    p <- p + 
+      ggplot2::geom_point(data = jitplo, ggplot2::aes(x = `Monitoring Location ID`, y = `Result Value`), position = ggplot2::position_dodge2(width = 0.7 * width), alpha = 0.5, size = 1)
     
   }
   
-  # barplot, fecal group
-  if(type == 'bar' & fecalgrp){
-    
-    toplo <- toplo %>% 
-      dplyr::group_by(`Monitoring Location ID`, `Result Attribute`)
-    
-    # get mean and CI summary
-    toplo <- utilMWRconfint(toplo, logscl = logscl)
-
-    p <- p +
-      ggplot2::geom_bar(data = toplo, ggplot2::aes(x = `Result Attribute`, y = `Result Value`), 
-                        fill = fill, stat = 'identity', alpha = alpha, width = width) + 
-      ggplot2::facet_grid(~`Monitoring Location ID`)
-    
-    if(confint)
-      p <- p +
-        ggplot2::geom_errorbar(data = toplo, ggplot2::aes(x = `Result Attribute`, ymin = lov, ymax = hiv), width = 0.2 * width)
-    
-  }
-  
-  # jitter if box
-  if(jitter & type == 'box'){
+  # jitter if box, fecalgroup
+  if(type == 'jitterbox' & fecalgrp){
     
     jitplo <- toplo %>% 
       dplyr::filter(!outlier)
     
     p <- p + 
       ggplot2::geom_point(data = jitplo, ggplot2::aes(x = `Result Attribute`, y = `Result Value`), position = ggplot2::position_dodge2(width = 0.7 * width), alpha = 0.5, size = 1)
+    
+  }
+  
+  # barplot
+  if((type == 'bar' | type == 'jitterbar') & !fecalgrp){
+    
+    toplo <- toplo %>% 
+      dplyr::group_by(`Monitoring Location ID`)
+    
+    # get mean and CI summary
+    toplobr <- utilMWRconfint(toplo, logscl = logscl)
+    
+    p <- p +
+      ggplot2::geom_bar(data = toplobr, ggplot2::aes(x = `Monitoring Location ID`, y = `Result Value`),
+                        fill = fill, stat = 'identity', alpha = alpha, width = width)
+    
+    if(confint)
+      p <- p + 
+        ggplot2::geom_errorbar(data = toplobr, ggplot2::aes(x = `Monitoring Location ID`, ymin = lov, ymax = hiv), width = 0.2 * width)
+    
+  }
+  
+  # barplot, fecal group
+  if((type == 'bar' | type == 'jitterbar') & fecalgrp){
+    
+    toplo <- toplo %>% 
+      dplyr::group_by(`Monitoring Location ID`, `Result Attribute`)
+    
+    # get mean and CI summary
+    toplobr <- utilMWRconfint(toplo, logscl = logscl)
+
+    p <- p +
+      ggplot2::geom_bar(data = toplobr, ggplot2::aes(x = `Result Attribute`, y = `Result Value`), 
+                        fill = fill, stat = 'identity', alpha = alpha, width = width) + 
+      ggplot2::facet_grid(~`Monitoring Location ID`)
+    
+    if(confint)
+      p <- p +
+        ggplot2::geom_errorbar(data = toplobr, ggplot2::aes(x = `Result Attribute`, ymin = lov, ymax = hiv), width = 0.2 * width)
+    
+  }
+  
+  if(type %in% c('jitterbar', 'jitter') & !fecalgrp){
+    
+    p <- p + 
+      ggplot2::geom_point(data = toplo, ggplot2::aes(x = `Monitoring Location ID`, y = `Result Value`), position = ggplot2::position_dodge2(width = 0.7 * width), alpha = 0.5, size = 1)
+    
+  }
+  
+  if(type %in% c('jitterbar', 'jitter') & fecalgrp){
+    
+    p <- p +
+      ggplot2::geom_point(data = toplo, ggplot2::aes(x = `Result Attribute`, y = `Result Value`), position = ggplot2::position_dodge2(width = 0.7 * width), alpha = 0.5, size = 1) + 
+      ggplot2::facet_grid(~`Monitoring Location ID`)
     
   }
   
--- tests/testthat/test-anlzMWRsite.R
+++ tests/testthat/test-anlzMWRsite.R
@@ -17,15 +17,15 @@ test_that("Checking param is a fecal indicator if fecalgrp is TRUE", {
   expect_error(anlzMWRsite(res = resdat, param = 'DO', acc = accdat, thresh = "fresh", fecalgrp = TRUE))
 })
 
-test_that("Checking output format boxplot, fecalgrp", {
-  result <- anlzMWRsite(res = resdat, param = 'E.coli', acc = accdat, thresh = "fresh", type = 'box', 
+test_that("Checking output format jittered boxplot, fecalgrp", {
+  result <- anlzMWRsite(res = resdat, param = 'E.coli', acc = accdat, thresh = "fresh", type = 'jitterbox', 
                         site = c('ABT-077', 'ABT-162', 'CND-009', 'CND-110', 'HBS-016', 'HBS-031'),
                         fecalgrp = TRUE)
   expect_s3_class(result, 'ggplot')
 })
 
-test_that("Checking output format barplot, fecalgrp", {
-  result <- anlzMWRsite(res = resdat, param = 'E.coli', acc = accdat, thresh = "fresh", type = 'bar', 
+test_that("Checking output format jittered barplot, fecalgrp", {
+  result <- anlzMWRsite(res = resdat, param = 'E.coli', acc = accdat, thresh = "fresh", type = 'jitterbar', 
                         site = c('ABT-077', 'ABT-162', 'CND-009', 'CND-110', 'HBS-016', 'HBS-031'),
                         fecalgrp = TRUE, confint = TRUE)
   expect_s3_class(result, 'ggplot')
