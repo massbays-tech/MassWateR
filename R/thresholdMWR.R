d1ca5e87fe51c67f668225804e85b45b12190d4d
fawda123 <mbafs2012@gmail.com>
2022-09-09 10:45:47 -0400

    update decimal places in thresholdMWR #32

--- R/thresholdMWR.R
+++ R/thresholdMWR.R
@@ -2,7 +2,7 @@
 #' 
 #' Master thresholds list for analysis of results data
 #' 
-#' @format A \code{data.frame} of 39 rows and 6 columns
+#' @format A \code{data.frame} of 28 rows and 10 columns
 #' 
 #' @details This file includes appropriate threshold values of water quality parameters for marine and freshwater environments based on state standards or typical ranges in Massachusetts.  
 #' 
@@ -15,7 +15,8 @@
 #'   rename(
 #'     `Simple Parameter` = Simple_Parameter,
 #'     uom = UOM
-#'   )
+#'   ) %>% 
+#'   mutate_if(is.numeric, round, 2)
 #' 
 #' save(thresholdMWR, file = 'data/threhsoldMWR.RData', compress = 'xz')
 #' }
