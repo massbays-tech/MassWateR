a4203b3d8ca68ab09968f8e00f59cd108db1562f
fawda123 <mbafs2012@gmail.com>
2022-11-08 17:21:25 -0500

    fixing test for tabMWRwqx

--- tests/testthat/test-tabMWRwqx.R
+++ tests/testthat/test-tabMWRwqx.R
@@ -1,4 +1,4 @@
 test_that("Verifying message output for wqx table creation", {
-  expect_message(tabMWRwqx(resdat, accdat, sitdat, wqxdat, warn = FALSE))
+  expect_message(tabMWRwqx(respth, accpth, sitpth, wqxpth, warn = FALSE))
   file.remove(file.path(getwd(), 'wqxtab.xlsx'))
 })
