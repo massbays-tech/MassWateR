103316fea0dbc56328a0b9c3e134e6e50ed9d735
fawda123 <mbafs2012@gmail.com>
2022-11-11 09:38:41 -0500

    update global variables and move rmarkdown form suggests to depends

--- R/globalVariables.R
+++ R/globalVariables.R
@@ -15,7 +15,24 @@ globalVariables(c("Activity Start Date", "Activity Start Time", "QC Reference Va
                   "hiv", "label", "lov", "meanval","num", "thresholdMWR", "degree", "geometry", 
                   "Location Group", "sitdat", "pondsMWR", "riversMWR", "streamsMWR", 
                   "Activity Depth/Height Measure", "kilometer", "Activity Relative Depth Name", 
-                  "Diff./Accuracy", "signv", "x", "y"))
+                  "Diff./Accuracy", "signv", "x", "y", "Activity Depth/Height Unit", 
+                  "Activity ID", "Activity Media Name", "Activity Start Time Zone",
+                  "Characteristic Name User Supplied", "Method Speciation", 
+                  "Monitoring Location Horizontal Collection Method", 
+                  "Monitoring Location Horizontal Coordinate Reference System",
+                  "Monitoring Location Latitude (DD.DDDD)", 
+                  "Monitoring Location Longitude (-DDD.DDDD)", "Monitoring Location Name",
+                  "Monitoring Location Source Map Scale (conditional)", "Monitoring Location Type",
+                  "Project ID", "Result Analytical Method Context", "Result Analytical Method ID",
+                  "Result Comment", "Result Detection Condition", 
+                  "Result Detection/Quantitation Limit Measure", 
+                  "Result Detection/Quantitation Limit Type", 
+                  "Result Detection/Quantitation Limit Unit", "Result Measure Qualifier",
+                  "Result Sample Fraction", "Result Status ID", "Result Value Type", 
+                  "Result Value2", "Sample Collection Equipment Name", 
+                  "Sample Collection Method Context", "Sample Collection Method ID", 
+                  "Tribal Land Indicator (Yes/No) (optional)", "Tribal Land Name (optional)",
+                  "actstrtm", "actyaid", "dateaid", "deptaid", "moniaid", "timeaid"))
 
 #' @importFrom stats IQR na.omit quantile t.test
 NULL
