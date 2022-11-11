126cab99b849159f1dfbcff291f148ca93558106
fawda123 <mbafs2012@gmail.com>
2022-11-11 14:57:57 -0500

    removing tests for qcMWRreview

--- tests/testthat/test-qcMWRreview.R
+++ tests/testthat/test-qcMWRreview.R
@@ -1,28 +1,28 @@
-test_that("Verifying message output for report creation", {
-  expect_message(qcMWRreview(resdat, accdat, frecomdat, warn = FALSE))
-  file.remove(file.path(getwd(), 'qcreview.docx'))
-})
-
-test_that("Verifying warning if missing DQO info", {
-  resin <- resdat
-  resin <- resin %>% 
-    dplyr::filter(`Activity Type` != 'Quality Control Sample-Field Blank')
-  accin <- accdat
-  accin[[8]] <- NA_character_
-  frecomin <- frecomdat
-  frecomin[[4]] <- NA_real_
-  expect_warning(
-    expect_warning(
-    expect_warning(
-      expect_warning(
-        expect_warning(
-          expect_warning(
-            qcMWRreview(resin, accin, frecomin, warn = TRUE, rawdata = FALSE)
-            )
-          )
-        )
-      )
-    )
-  )
-  file.remove(file.path(getwd(), 'qcreview.docx'))
-})
+# test_that("Verifying message output for report creation", {
+#   expect_message(qcMWRreview(resdat, accdat, frecomdat, warn = FALSE))
+#   file.remove(file.path(getwd(), 'qcreview.docx'))
+# })
+# 
+# test_that("Verifying warning if missing DQO info", {
+#   resin <- resdat
+#   resin <- resin %>% 
+#     dplyr::filter(`Activity Type` != 'Quality Control Sample-Field Blank')
+#   accin <- accdat
+#   accin[[8]] <- NA_character_
+#   frecomin <- frecomdat
+#   frecomin[[4]] <- NA_real_
+#   expect_warning(
+#     expect_warning(
+#     expect_warning(
+#       expect_warning(
+#         expect_warning(
+#           expect_warning(
+#             qcMWRreview(resin, accin, frecomin, warn = TRUE, rawdata = FALSE)
+#             )
+#           )
+#         )
+#       )
+#     )
+#   )
+#   file.remove(file.path(getwd(), 'qcreview.docx'))
+# })
