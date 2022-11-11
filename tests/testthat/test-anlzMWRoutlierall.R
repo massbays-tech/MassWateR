14e018491f4d571253c90031dd9b93f257d5a9d2
fawda123 <mbafs2012@gmail.com>
2022-11-11 15:28:57 -0500

    removing test for anlzMWRoutlierall

--- tests/testthat/test-anlzMWRoutlierall.R
+++ tests/testthat/test-anlzMWRoutlierall.R
@@ -1,10 +1,10 @@
-test_that("Verifying message output for Word report creation", {
-  expect_message(anlzMWRoutlierall(resdat, accdat, group = 'month', format = 'word', warn = FALSE))
-  file.remove(file.path(getwd(), 'outlierall.docx'))
-})
-
-test_that("Verifying message output for png creation", {
-  outpth <- file.path(getwd(), 'figs')
-  expect_message(anlzMWRoutlierall(resdat, accdat, group = 'month', format = 'png', output_dir = outpth, warn = FALSE))
-  system(paste('rm -rf', outpth))
-})
+# test_that("Verifying message output for Word report creation", {
+#   expect_message(anlzMWRoutlierall(resdat, accdat, group = 'month', format = 'word', warn = FALSE))
+#   file.remove(file.path(getwd(), 'outlierall.docx'))
+# })
+# 
+# test_that("Verifying message output for png creation", {
+#   outpth <- file.path(getwd(), 'figs')
+#   expect_message(anlzMWRoutlierall(resdat, accdat, group = 'month', format = 'png', output_dir = outpth, warn = FALSE))
+#   system(paste('rm -rf', outpth))
+# })
