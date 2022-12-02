c2888da618f97d54aca7f4cacff19f6316beb598
fawda123 <mbafs2012@gmail.com>
2022-12-02 16:13:24 -0500

    adding wqx input tests for utilMWRinput

--- tests/testthat/test-utilMWRinput.R
+++ tests/testthat/test-utilMWRinput.R
@@ -1,15 +1,15 @@
 test_that("Checking path input for res, acc, frecom, and sit", {
-  result <- utilMWRinput(res = respth, acc = accpth, frecom = frecompth, sit = sitpth, runchk = F)
+  result <- utilMWRinput(res = respth, acc = accpth, frecom = frecompth, sit = sitpth, wqx = wqxpth, runchk = F)
   expect_type(result, 'list')
 })
 
 test_that("Checking data frame input for res, acc, frecom, and sit", {
-  result <- utilMWRinput(res = resdat, acc = accdat, frecom = frecomdat, sit = sitdat)
+  result <- utilMWRinput(res = resdat, acc = accdat, frecom = frecomdat, sit = sitdat, wqx = wqxdat)
   expect_type(result, 'list')
 })
 
 test_that("Checking fset input", {
-  result <- utilMWRinput(fset = list(res = resdat, acc = accdat, frecom = frecomdat, sit = sitdat))
+  result <- utilMWRinput(fset = list(res = resdat, acc = accdat, frecom = frecomdat, sit = sitdat, wqx = wqxdat))
   expect_type(result, 'list')
 })
 
@@ -29,3 +29,6 @@ test_that("Checking missing file for path for sit", {
   expect_error(utilMWRinput(sit = '~/test'))
 })
 
+test_that("Checking missing file for path for wqx", {
+  expect_error(utilMWRinput(wqx = '~/test'))
+})
