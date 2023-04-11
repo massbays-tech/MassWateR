f0d3c407fe0a9b2645d029bf6483315cb8b9c9b7
fawda123 <mbafs2012@gmail.com>
2023-04-11 10:10:19 -0400

    fix tabMWRwqx test

--- tests/testthat/test-tabMWRwqx.R
+++ tests/testthat/test-tabMWRwqx.R
@@ -16,5 +16,5 @@ test_that("Check warning if UQL or MDL missing from accdat", {
 test_that("Verifying message output for wqx table creation if project id and sample collection method id missing", {
   reschk <- resdat %>% 
     select(-`Project ID`, -`Sample Collection Method ID`)
-  expect_warning(expect_message(reschk, accpth, sitpth, wqxpth, warn = TRUE, output_dir = tempdir(), runchk = F))
+  expect_warning(expect_message(tabMWRwqx(reschk, accpth, sitpth, wqxpth, warn = TRUE, output_dir = tempdir(), runchk = F)))
 })
