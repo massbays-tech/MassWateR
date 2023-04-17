ccf40d5246bedaf49ab8a468668954b3467cb279
fawda123 <mbafs2012@gmail.com>
2023-04-17 09:34:53 -0400

    fixing test for readMWRresultsview

--- tests/testthat/test-readMWRresultsview.R
+++ tests/testthat/test-readMWRresultsview.R
@@ -3,5 +3,5 @@ test_that("Verifying message output for report creation", {
 })
 
 test_that("Checking error if incorrect value in columns", {
-  expect_error(readMWRresultsview(respth, columns = c("Characeristic Name"), outputdir = tempdir()))
+  expect_error(readMWRresultsview(respth, columns = c("Characeristic Name"), output_dir = tempdir()))
 })
