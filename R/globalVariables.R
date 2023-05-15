377542bcb89b13d42b80cd5df99c194521643e2d
fawda123 <mbafs2012@gmail.com>
2023-05-14 21:04:09 -0400

    adding to global vars

--- R/globalVariables.R
+++ R/globalVariables.R
@@ -37,7 +37,7 @@ globalVariables(c("Activity Start Date", "Activity Start Time", "QC Reference Va
                   "Field Duplicate2", "Field Duplicatena", "Lab Blankna", "Lab Duplicate2",
                   "Lab Duplicatena", "Sampling Method Context", "Spike/Check Accuracy2", 
                   "Spike/Check Accuracyna", "Result Detection/Quantitation Limit Value",
-                  "Local Record ID"
+                  "Local Record ID", "Activity ID User Supplied"
                   ))
 
 #' @importFrom stats IQR na.omit quantile t.test
