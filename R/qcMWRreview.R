0a1d5095e6748f7b266f53486746d17a016e929d
fawda123 <mbafs2012@gmail.com>
2025-02-16 12:25:22 -0500

    add cens as input to qcMWRreview

--- R/qcMWRreview.R
+++ R/qcMWRreview.R
@@ -3,7 +3,8 @@
 #' @param res character string of path to the results file or \code{data.frame} for results returned by \code{\link{readMWRresults}}
 #' @param acc character string of path to the data quality objectives file for accuracy or \code{data.frame} returned by \code{\link{readMWRacc}}
 #' @param frecom character string of path to the data quality objectives file for frequency and completeness or \code{data.frame} returned by \code{\link{readMWRfrecom}}
-#' @param fset optional list of inputs with elements named \code{res}, \code{acc}, \code{frecom}, \code{sit}, or \code{wqx} overrides the other arguments
+#' @param cens character string of path to the censored data file or \code{data.frame} returned by \code{\link{readMWRcens}}
+#' @param fset optional list of inputs with elements named \code{res}, \code{acc}, \code{frecom}, and \code{cens}, overrides the other arguments
 #' @param output_dir character string of the output directory for the rendered file
 #' @param output_file optional character string for the file name
 #' @param rawdata logical to include quality control accuracy summaries for raw data, e.g., field blanks, etc.
@@ -22,11 +23,11 @@
 #' 
 #' The function compiles a review report as a Word document for all quality control checks included in the MassWateR package.  The report shows several tables, including the data quality objectives files for accuracy, frequency, and completeness, summary results for all accuracy checks, summary results for all frequency checks, summary results for all completeness checks, and individual results for all accuracy checks.  The report uses the individual table functions (which can be used separately) to return the results, which include \code{\link{tabMWRacc}}, \code{\link{tabMWRfre}}, and \code{\link{tabMWRcom}}.  The help files for each of these functions can be consulted for a more detailed explanation of the quality control checks. 
 #' 
-#' The workflow for using this function is to import the required data (results and data quality objective files) and to fix any errors noted on import prior to creating the review report.  Additional warnings that may be of interest as returned by the individual table functions can be returned in the console by setting \code{warn = TRUE}.  
+#' The workflow for using this function is to import the required data (results, data quality objective, and censored files) and to fix any errors noted on import prior to creating the review report.  Additional warnings that may be of interest as returned by the individual table functions can be returned in the console by setting \code{warn = TRUE}.  
 #' 
-#' Optional arguments that can be changed as needed include specifying the file name with \code{output_file}, suppressing the raw data summaries at the end of the report with \code{rawdata = FALSE}, and changing the table font sizes (\code{dqofontsize} for the data quality objectives on the first page, \code{tabfontsize} for the remainder).
+#' Optional arguments that can be changed as needed include specifying the file name with \code{output_file}, suppressing the raw data summaries at the end of the report with \code{rawdata = FALSE}, and changing the table font sizes (\code{dqofontsize} for the data quality objectives on the first page, \code{tabfontsize} for the remainder).  Set \code{savesheet = TRUE} to also save a spreadsheet of the tables in the report.
 #' 
-#' The function can be used with inputs as paths to the relevant files or as data frames returned by \code{\link{readMWRresults}}, \code{\link{readMWRacc}}, and \code{\link{readMWRfrecom}}.  For the former, the full suite of data checks can be evaluated with \code{runkchk = T} (default) or suppressed with \code{runchk = F}, as explained in the relevant help files.  In the latter case, downstream analyses may not work if data are formatted incorrectly. For convenience, a named list with the input arguments as paths or data frames can be passed to the \code{fset} argument instead. See the help file for \code{\link{utilMWRinput}}.
+#' The function can be used with inputs as paths to the relevant files or as data frames returned by \code{\link{readMWRresults}}, \code{\link{readMWRacc}}, \code{\link{readMWRfrecom}}, and \code{\link{readMWRcens}}.  For the former, the full suite of data checks can be evaluated with \code{runkchk = T} (default) or suppressed with \code{runchk = F}, as explained in the relevant help files.  In the latter case, downstream analyses may not work if data are formatted incorrectly. For convenience, a named list with the input arguments as paths or data frames can be passed to the \code{fset} argument instead. See the help file for \code{\link{utilMWRinput}}.
 #' 
 #' @examples
 #' # results data path
@@ -38,6 +39,9 @@
 #' # dqo completeness data path
 #' frecompth <- system.file('extdata/ExampleDQOFrequencyCompleteness.xlsx', package = 'MassWateR')
 #' 
+#' # censored data path
+#' censpth <- system.file('extdata/ExampleCensored.xlsx', package = 'MassWateR')
+#' 
 #' # results data
 #' resdat <- readMWRresults(respth)
 #' 
@@ -47,11 +51,14 @@
 #' # frequency and completeness data
 #' frecomdat <- readMWRfrecom(frecompth)
 #' 
+#' # censored data
+#' censdat <- readMWRcens(censpth)
+#' 
 #' \donttest{
 #' # create report
-#' qcMWRreview(res = resdat, acc = accdat, frecom = frecomdat, output_dir = tempdir())
+#' qcMWRreview(res = resdat, acc = accdat, frecom = frecomdat, cens = censdat, output_dir = tempdir())
 #' }
-qcMWRreview <- function(res = NULL, acc = NULL, frecom = NULL, fset = NULL, output_dir, output_file = NULL, savesheet = FALSE, rawdata = TRUE, dqofontsize = 7.5, tabfontsize = 9, padding = 0, warn = TRUE, runchk = TRUE) {
+qcMWRreview <- function(res = NULL, acc = NULL, frecom = NULL, cens = NULL, fset = NULL, output_dir, output_file = NULL, savesheet = FALSE, rawdata = TRUE, dqofontsize = 7.5, tabfontsize = 9, padding = 0, warn = TRUE, runchk = TRUE) {
 
   utilMWRinputcheck(mget(ls()))
 
@@ -59,10 +66,11 @@ qcMWRreview <- function(res = NULL, acc = NULL, frecom = NULL, fset = NULL, outp
   qcreview <- system.file('rmd', 'qcreview.Rmd', package = 'MassWateR')
   
   # get input 
-  inp <- utilMWRinput(res = res, acc = acc, frecom = frecom, fset = fset, runchk = runchk, warn = warn)
+  inp <- utilMWRinput(res = res, acc = acc, frecom = frecom, cens = cens, fset = fset, runchk = runchk, warn = warn)
   resdat <- inp$resdat
   accdat <- inp$accdat
   frecomdat <- inp$frecomdat
+  censdat <- inp$censdat
   
   # date for report
   resdatdt <- range(resdat$`Activity Start Date`)
@@ -101,7 +109,7 @@ qcMWRreview <- function(res = NULL, acc = NULL, frecom = NULL, fset = NULL, outp
     thmsum(wd = wd, fontname = fontname)
     
   # completeness table
-  tabcom <- tabMWRcom(res = resdat, frecom = frecomdat, warn = warn, parameterwd = 1.15, noteswd = 2) %>% 
+  tabcom <- tabMWRcom(res = resdat, frecom = frecomdat, cens = censdat, warn = warn, parameterwd = 1.15, noteswd = 2) %>% 
     flextable::width(width = (wd - 3.15) / (flextable::ncol_keys(.) - 2), j = 2:(flextable::ncol_keys(.) - 1)) %>%
     flextable::font(fontname = fontname, part = 'all')
 
